d106fdd515f95e67b42edadda6e2cb14f1a0887a
Marcela <rivas.lina@correounivalle.edu.co>
2024-03-02 11:47:56 -0500

    barrita de asesor juridico

--- src/pages/IADiciplinaria.jsx
+++ src/pages/IADiciplinaria.jsx
@@ -3,7 +3,17 @@ import React from "react";
 const IADiciplinaria = () => {
   return (
     <div >
-      {"hola mundo"}
+      <section >
+        <p>Asesor Diciplinario</p>
+      </section>  
+      <section >
+        <input type= "text" placeholder="Mensaje ..." />
+        <button>Enviar</button>
+      </section> 
+        <div >
+        
+        </div>
+      
     </div>
   );
 }
--- src/pages/IAJuridica.jsx
+++ src/pages/IAJuridica.jsx
@@ -3,7 +3,17 @@ import React from "react";
 const IAJuridica = () => {
   return (
     <div >
-      {"hola mundo"}
+      <section >
+        <p>Asesor Juridico</p>
+      </section>  
+      <section className="mensaje">
+        <input type= "text" placeholder="Mensaje ..." />
+        <button>Enviar</button>
+      </section> 
+        <div >
+        
+        </div>
+      
     </div>
   );
 }
