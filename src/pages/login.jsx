9795c7872228c5355167aa185ad4f3f968ea1b77
Marcela <rivas.lina@correounivalle.edu.co>
2024-03-02 12:38:43 -0500

    nn

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Login = () => {
-   
+  
 
     return (
         <div >
@@ -18,8 +18,12 @@ const Login = () => {
                         <label htmlFor=""className="absolute text-sm duration-300 transform -translate-y-3 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:focus:border-blue-500 peer-placeholder-shown:scale-100 peer-focus:scale-75 peer-focus:translate-y-6">Password</label>
                         </div>
                         <div>
-                           <button type="submit" className="w-full mb-4 text-[20px] rounded blue-500 py-2 hover:bg-blue-600 translation-colors duration-300" >Ingresar</button>  
-                            </div>                   
+                           <button type="submit" className="w-full mb-4 text-[20px] rounded blue-500 py-2 hover:bg-blue-600 translation-colors duration-300"
+                            >
+                                Ingresar
+                           </button> 
+                        </div>   
+                                            
                     </div>
                 </form>
                 </div>
