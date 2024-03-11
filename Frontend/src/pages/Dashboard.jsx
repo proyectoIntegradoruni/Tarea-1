a062775d2bc6ffde1dc61e1c8f217c93c5472a85
Marcela <rivas.lina@correounivalle.edu.co>
2024-03-10 21:23:52 -0500

    dasboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Dashboard = () => {
     
     return (
-        <div style={{ fontFamily: 'Taiwin', textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
+        <div style={{ fontFamily: 'Taiwin', textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
             <h1>
             Bienvenido a la plataforma de asesoría jurídica y disciplinaria
             </h1>
@@ -14,7 +14,7 @@ const Dashboard = () => {
                {' '}Por favor, elige alguno de nuestros asesores de la barra lateral izquierda{' '}
             
             </h1>
-            <img src="https://res.cloudinary.com/dte7upwcr/image/upload/v1/blog/blog2/mejores-herramientas-chat-online/mejores-herramientas-chat-online-img_header.jpg" alt="Descripción de la imagen" style={{ width: '100%', maxWidth: '80%', margin: '40px 0' }} />
+            <img src="https://res.cloudinary.com/dte7upwcr/image/upload/v1/blog/blog2/mejores-herramientas-chat-online/mejores-herramientas-chat-online-img_header.jpg" alt="Descripción de la imagen" style={{ width: '100%', maxWidth: '90%', margin: '40px 0' }} />
         </div>
     );
 };
