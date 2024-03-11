16e6699d6ef89bb2dbb1c0dc9a925303acfae980
Marcela <rivas.lina@correounivalle.edu.co>
2024-03-10 22:16:23 -0500

    diseño dasboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -3,18 +3,29 @@ import React from 'react';
 const Dashboard = () => {
     
     return (
-        <div style={{ fontFamily: 'Taiwin', textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-            <h1>
-            Bienvenido a la plataforma de asesoría jurídica y disciplinaria
+        <div style={{ position: "absolute", display: 'flex', height: '100vh',  }}>
+           <div style={{ fontFamily: 'Taiwin', textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop : "64px"}}>
+            <h1 style={{fontSize: '84px' , fontFamily: 'Comic Sans MS', fontWeight: 'bold', display: 'inline-block', margin: '10'}}>
+            Bienvenido a la 
+            <br />
+            plataforma de 
+            asesoría jurídica y 
+            disciplinaria
             </h1>
             <h1  style={{ fontSize: '32px' }}>
-                <span style={{ fontSize: '36px', transform: 'rotate(360deg)', display: 'inline-block' }}>
+                <span style={{ fontSize: '64px', transform: 'rotate(360deg)', display: 'inline-block' }}>
                     &larr;
                 </span>
-               {' '}Por favor, elige alguno de nuestros asesores de la barra lateral izquierda{' '}
+                <br />
+               {' '}Por favor, elige alguno de nuestros asesores 
+               <br />de la barra lateral izquierda{' '}
             
             </h1>
-            <img src="https://res.cloudinary.com/dte7upwcr/image/upload/v1/blog/blog2/mejores-herramientas-chat-online/mejores-herramientas-chat-online-img_header.jpg" alt="Descripción de la imagen" style={{ width: '100%', maxWidth: '90%', margin: '40px 0' }} />
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'flex-end', width: '100%', height: '100%' }}>
+                <img src="../../public/imagenInicio.gif" alt="Descripción de la imagen" style={{ width: '100%', height: '100%', objectFit: 'cover'}} />
+            </div>
+            
         </div>
     );
 };
